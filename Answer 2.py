b3707675a5b89ef268a6705cc1fd811dc3b12a25
Abhishek Paul <abhishek.paul.min15@itbhu.ac.in>
2018-04-05 19:44:57 +0530

    Add files via upload

---
+++ Answer 2.py
@@ -0,0 +1,26 @@
+# cook your dish here   
+def factorial(n):
+    p = n
+    a = 0
+    while n>=1:
+        P=P*(n-1)
+        n=n-1
+    return P   
+    
+def permutation_of_letters(A):
+    n = len(A))
+    if n==1:
+        return str(A)
+    else:
+        return permutation_of_letters(A[1:]) + permutation_of_letters(A[-1:-n])
+    output.append    
+
+def dict_rank(Letters, word):
+    A = str(Letters)
+    n = factorial(len(Letters))
+    B = []
+    C = []
+    for i in range(n):
+        for j in range(len(Letters)):
+            if j in 
+    
